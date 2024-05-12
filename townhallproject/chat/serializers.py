005a80250084de38fe3bec061c5557b452ccc6dc
Tom Archer <tomarcher@Toms-MacBook-Pro-16.local>
2024-05-12 15:28:31 +1000

    Add the id field to the posts endpoint.

--- townhallproject/chat/serializers.py
+++ townhallproject/chat/serializers.py
@@ -1,7 +1,8 @@
 from chat import models
 from rest_framework import serializers
 
+
 class PostSerializer(serializers.HyperlinkedModelSerializer):
-  class Meta:
-    model = models.Post
-    fields = ['content', 'poster_name', 'created_at']
+    class Meta:
+        model = models.Post
+        fields = ['id', 'content', 'poster_name', 'created_at']
